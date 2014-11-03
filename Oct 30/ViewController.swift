f0cc97b428c44760d705dc9d7790a4aa6f035988
Elena Da Re <elenadare@Elenas-MacBook-Pro.local>
2014-11-02 19:59:25 -0500

    Initial Commit

---
+++ Oct 30/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oct 30
+//
+//  Created by Elena Da Re on 11/2/14.
+//  Copyright (c) 2014 Elena Da Re. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
